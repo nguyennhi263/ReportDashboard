3382becf098942d164308c59756baf1d89640998
SN707 <32unix@gmail.com>
2018-06-22 10:53:07 +0700

    LogInButton

--- app/src/main/java/vn/com/ifca/reportdashboard/Activities/LogInActivity.java
+++ app/src/main/java/vn/com/ifca/reportdashboard/Activities/LogInActivity.java
@@ -2,17 +2,20 @@ package vn.com.ifca.reportdashboard.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import vn.com.ifca.reportdashboard.R;
 
 public class LogInActivity extends AppCompatActivity {
     private EditText user;
     private EditText pass;
-    private TextView userdisplay;
-    private TextView passdisplay;
+    private CardView LoginBtn;
     private String username;
     private String password;
     @Override
@@ -21,15 +24,23 @@ public class LogInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_log_in);
         user = findViewById(R.id.txtUsername);
         pass = findViewById(R.id.txtPassword);
-        //userdisplay = findViewById(R.id.username);
-        //passdisplay = findViewById(R.id.password);
-    }
-    public void LogInButton(View view){
-        username = user.getText().toString();
-        password = pass.getText().toString();
-        setContentView(R.layout.activity_main);
-        //userdisplay.setText(username);
-        //passdisplay.setText(password);
-
+        LoginBtn = findViewById(R.id.LoginBtn);
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = user.getText().toString();
+                password = pass.getText().toString();
+                if (TextUtils.isEmpty(username))
+                {
+                    Toast.makeText(LogInActivity.this, "Enter your username!", Toast.LENGTH_SHORT).show();
+                }
+                else if (TextUtils.isEmpty(password)){
+                    Toast.makeText(LogInActivity.this, "Enter your password!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Hello " + username + "!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
