17959103eefaeba96d92c01160c2ed2827520d01
SN707 <32unix@gmail.com>
2018-06-22 10:04:31 +0700

    LogInScreen

--- app/src/main/java/vn/com/ifca/reportdashboard/Activities/LogInActivity.java
+++ app/src/main/java/vn/com/ifca/reportdashboard/Activities/LogInActivity.java
@@ -2,14 +2,34 @@ package vn.com.ifca.reportdashboard.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import vn.com.ifca.reportdashboard.R;
 
 public class LogInActivity extends AppCompatActivity {
-
+    private EditText user;
+    private EditText pass;
+    private TextView userdisplay;
+    private TextView passdisplay;
+    private String username;
+    private String password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        user = findViewById(R.id.txtUsername);
+        pass = findViewById(R.id.txtPassword);
+        //userdisplay = findViewById(R.id.username);
+        //passdisplay = findViewById(R.id.password);
+    }
+    public void LogInButton(View view){
+        username = user.getText().toString();
+        password = pass.getText().toString();
+        setContentView(R.layout.activity_main);
+        //userdisplay.setText(username);
+        //passdisplay.setText(password);
+
     }
 }
