f68dfb0770f9be83e4d2bd37ab1324f402899953
Hello <gnguyen19@ssis.edu.vn>
2018-06-22 13:24:12 +0700

    Language options layout edit

--- app/src/main/java/vn/com/ifca/reportdashboard/Activities/SettingActivity.java
+++ app/src/main/java/vn/com/ifca/reportdashboard/Activities/SettingActivity.java
@@ -3,6 +3,8 @@ package vn.com.ifca.reportdashboard.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import vn.com.ifca.reportdashboard.R;
+
 public class SettingActivity extends AppCompatActivity {
 
     @Override
