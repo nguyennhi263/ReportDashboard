ac509c01f84c8ef8029047bafc1cb2dc2100ed04
SN707 <32unix@gmail.com>
2018-06-25 10:40:37 +0700

    setting language

--- app/src/main/java/vn/com/ifca/reportdashboard/Activities/ReportDetailActivity.java
+++ app/src/main/java/vn/com/ifca/reportdashboard/Activities/ReportDetailActivity.java
@@ -5,15 +5,13 @@ import android.os.Bundle;
 
 import vn.com.ifca.reportdashboard.R;
 
-<<<<<<< HEAD:app/src/main/java/vn/com/ifca/reportdashboard/Activities/SettingActivity.java
 public class SettingActivity extends AppCompatActivity {
-=======
-public class ReportDetailActivity extends AppCompatActivity {
->>>>>>> feb19d797589ade4739a28af257e03480da04012:app/src/main/java/vn/com/ifca/reportdashboard/Activities/ReportDetailActivity.java
+    public class ReportDetailActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_report_detail);
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_report_detail);
+        }
+}
 }
--- app/src/main/java/vn/com/ifca/reportdashboard/MainActivity.java
+++ app/src/main/java/vn/com/ifca/reportdashboard/MainActivity.java
@@ -10,6 +10,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import java.util.HashMap;
@@ -17,10 +19,14 @@ import java.util.HashMap;
 import vn.com.ifca.reportdashboard.Activities.LogInActivity;
 import vn.com.ifca.reportdashboard.Model.Database;
 import vn.com.ifca.reportdashboard.Model.IP4V;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     IP4V link;
     HashMap<String, String> ip;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +34,14 @@ public class MainActivity extends AppCompatActivity {
         link = new IP4V(getApplicationContext());
 
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.sidebar, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -46,69 +54,90 @@ public class MainActivity extends AppCompatActivity {
                 break;
             // action with ID action_settings was
             case R.id.nav_language:
-                Toast.makeText(this,
-                        "Language"
-                        , Toast.LENGTH_SHORT)
-                        .show();
-                break;
-            case R.id.nav_url:
-                Toast.makeText(this,
-                        "URL"
-                        , Toast.LENGTH_SHORT)
-                        .show();
+                final Dialog languageContractor = new Dialog(MainActivity.this);
+                languageContractor.setContentView(R.layout.language_options);
+                languageContractor.setCancelable(true);
+                Button cancelBtn = findViewById(R.id.cancel_language);
+                Button okBtn = findViewById(R.id.confirm_language);
+                boolean exit = false;
+                RadioGroup languageChoice = findViewById(R.id.languageSelection);
+                while (!exit) {
+                    int radioBtnId = languageChoice.getCheckedRadioButtonId();
+                    final RadioButton chosen = findViewById(radioBtnId);
+                    okBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (chosen.getText().toString().equals("")) {
+                                Toast.makeText(getApplicationContext(), "@string/languageWarning", Toast.LENGTH_SHORT).show();
+                            } else if (chosen.getText().toString().equals("English")) {
 
-                break;
-            case R.id.nav_url:
-                final Dialog dialogContractor = new Dialog(MainActivity.this);
-                dialogContractor.setContentView(R.layout.url_options);
-                dialogContractor.setCancelable(true);
-                // initial
-                Button cancelBtn = (Button) dialogContractor.findViewById(R.id.cancel_url);
-                final Button urlBtn = dialogContractor.findViewById(R.id.ok_url_entry);
-                final EditText urtText = (EditText) dialogContractor.findViewById(R.id.url_entry) ;
-                //get url in db
-                ip = link.getIP();
-                final String URL = ip.get(link.KEY_URL);
-                urtText.setText(URL);
-
-               // Save Event
-                urlBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (urlBtn.getText().toString().equals("")) {
+                                languageContractor.dismiss();
+                                exit = true;
+                            } else {
+                                languageContractor.dismiss();
+                                exit = true;
+                            }
 
                         }
-                        else
-                        {
-                            final String urlText = urtText.getText().toString();
-                            link.add_KEYURL(urlText);
-                            Toast.makeText(getApplicationContext(), "Updated URL", Toast.LENGTH_SHORT).show();
+                    });
+                    // Cancel
+                    cancelBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            languageContractor.dismiss();
+                            exit = true;
+                        }
+                    });
+                }
+                break;
+
+                case R.id.nav_url:
+                    final Dialog dialogContractor = new Dialog(MainActivity.this);
+                    dialogContractor.setContentView(R.layout.url_options);
+                    dialogContractor.setCancelable(true);
+                    // initial
+                    cancelBtn = (Button) dialogContractor.findViewById(R.id.cancel_url);
+                    final Button urlBtn = dialogContractor.findViewById(R.id.ok_url_entry);
+                    final EditText urtText = (EditText) dialogContractor.findViewById(R.id.url_entry);
+                    //get url in db
+                    ip = link.getIP();
+                    final String URL = ip.get(link.KEY_URL);
+                    urtText.setText(URL);
+
+                    // Save Event
+                    urlBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (urlBtn.getText().toString().equals("")) {
+
+                            } else {
+                                final String urlText = urtText.getText().toString();
+                                link.add_KEYURL(urlText);
+                                Toast.makeText(getApplicationContext(), "Updated URL", Toast.LENGTH_SHORT).show();
+                                dialogContractor.dismiss();
+                            }
+
+                        }
+                    });
+                    // Cancel
+                    cancelBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
                             dialogContractor.dismiss();
                         }
+                    });
+                    dialogContractor.show();
+                    break;
 
-                    }
-                });
-                // Cancel
-                cancelBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialogContractor.dismiss();
-                    }
-                });
-                dialogContractor.show();
-                break;
-            case R.id.nav_logout:
-                Toast.makeText(this,
-                        "Logout"
-                        , Toast.LENGTH_SHORT)
-                        .show();
-                break;
+                    case R.id.nav_logout:
+                        Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                        break;
 
-            default:
-                break;
+                    default:
+                        break;
 
+                }
+
+                return true;
         }
-
-        return true;
     }
-}
